6e5101be989e118560c466991f691de575ef100c
Fábio Chimanski <132751691+FabioChimanski@users.noreply.github.com>
2023-08-12 06:43:03 -0300

    ATT AULAS

---
+++ Curso JS/aula011/aula008.js
@@ -0,0 +1,10 @@
+var vel = 50.5
+console.log(`A velociadade do seu carro é ${vel}Km/h`)
+
+if (vel > 60){//Condição Simples
+    console.log(`Você ultrapassou a velocidade permitada. MULTADO!`)
+    console.log(`Dirija sempre usando cindo de segurança!`)}
+
+    console.log(`Você está dento da velocidade permitida pela da via`)
+    console.log(`Dirija sempre usando cindo de segurança!`)   
+
