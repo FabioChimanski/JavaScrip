f1aceefa1606f5bcf98592cb5a64d6fb0e42885f
Fábio Chimanski <132751691+FabioChimanski@users.noreply.github.com>
2023-08-15 22:42:01 -0300

    ATT AULA

---
+++ Curso JS/aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 19
+console.log(`Você tem ${idade} anos de idade!`)
+if (idade < 16){
+    console.log('Não vota')
+}else if(idade < 18){
+        console.log('Voto Opcional')
+}else{
+    console.log('Voto Obrigatorio')
+}
---
+++ Curso JS/aula012/ex012.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+
+if (hora < 7){
+console.log('Boa madrugada!')
+} else if (hora < 12){
+console.log('Boa dia!')
+} else if (hora < 18){
+console.log('Boa tarde!')
+} else if (hora < 23) {
+    console.log('Boa noite!')
+}
