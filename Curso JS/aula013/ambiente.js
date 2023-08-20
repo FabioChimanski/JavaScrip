1b217c668810a59d0f30e277135ae8923d523ecd
Fábio Chimanski <132751691+FabioChimanski@users.noreply.github.com>
2023-08-20 09:49:08 -0300

    aulas

---
+++ Curso JS/aula013/ambiente.js
@@ -0,0 +1,18 @@
+//Estrutura de repetição com teste logico no INICIO
+var c = 1
+
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+
+
+//Estrutura de repetição com teste logico no FINAL
+var c = 1
+
+do{
+    console.log(`Passo ${c}`)
+    c++
+} while ( (c <= 5) )
+
+}
+
