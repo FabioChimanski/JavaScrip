b918d1b77fb94a27146bd6cd0f7d699f299c7e1c
Fábio Chimanski <132751691+FabioChimanski@users.noreply.github.com>
2023-08-22 20:52:23 -0300

    att aula 15

---
+++ Curso JS/aula015/ambiente.js
@@ -0,0 +1,11 @@
+let num = [5,8,3]
+
+num.push(1) //adiciona um novo valor a variavel, em ultima possição
+num[5] = 7 //adiciona um novo valor a nossa variavel
+
+num.sort() //Organiza os elementos em ordem crecentes
+
+console.log(num)
+console.log(`O primeiro valor do vetor é ${num[1]}`) //monstra o primeiro numero do nosso vetor
+console.log(`O nosso vetor tem ${num.length} posições`)  //conta a quantidade de vetores (é um atributo, sendo assim não usa parenteses)
+
